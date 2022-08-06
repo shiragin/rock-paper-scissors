a317826420a450fd233fdefe7d0f814029340c95
Shira Ginat <shiragin@gmail.com>
2022-08-07 02:57:22 +0300

    Create getComputerChoice function + basic css

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 "use strict";
 
+const choice = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+    return choice[Math.floor(Math.random() * choice.length)];
+}
+
+console.log(getComputerChoice());
 
