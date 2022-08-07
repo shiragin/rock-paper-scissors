6abb054511a58afe35daf9a58da3f3e54ae59991
Shira Ginat <shiragin@gmail.com>
2022-08-07 10:32:33 +0300

    Improve playRound function code, add comments

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 "use strict";
 
+// Makes sure the playerChoice string is all lowercase except first letter to properly match the choice array
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+// Propmpt the player to chooce between rock paper and scissors and returns the choice
 function getPlayerChoice() {
     let playerChoice = prompt("Choose - Paper, Rock or Scissors?");
     playerChoice = capitalizeFirstLetter(playerChoice);
@@ -15,37 +17,49 @@ function getPlayerChoice() {
     }
 }
 
+// Randomly chooses the computerChoice out of the choice array
 function getComputerChoice() {
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
+// Plays one playRound, comparing the playerChoice and the computerChoice and returning the result
 function playRound(playerChoice, computerChoice) {
     console.log(`You chose... ${playerChoice}!`);
     console.log(`The evil computer chose... ${computerChoice}!`);
     if (playerChoice === computerChoice) {
         return `${playerChoice} vs. ${computerChoice}! It's a draw!`;
+    } else if (
+        (playerChoice === "Rock" && computerChoice === "Scissors") ||
+        (playerChoice === "Scissors" && computerChoice === "Paper") || (playerChoice === "Paper" && computerChoice === "Rock")
+    ) {
+        return `${playerChoice} vs. ${computerChoice}! You win!`;
     } else {
-        if (playerChoice === "Rock") {
-            if (computerChoice === "Paper") {
-                return `${playerChoice} vs. ${computerChoice}! You lose!`;
-            } else {
-                return `${playerChoice} vs. ${computerChoice}! You win!`;
-            }
-        } else if (playerChoice === "Paper") {
-            if (computerChoice === "Rock") {
-                return `${playerChoice} vs. ${computerChoice}! You win!`;
-            } else {
-                return `${playerChoice} vs. ${computerChoice}! You lose!`;
-            }
-        } else {
-            if (computerChoice === "Rock") {
-                return `${playerChoice} vs. ${computerChoice}! You lose!`;
-            } else {
-                return `${playerChoice} vs. ${computerChoice}! You win!`;
-            }
-        }
+        return `${playerChoice} vs. ${computerChoice}! You lose!`;
     }
 }
+
+// Previous version of playround code - so clumsy!
+
+// if (playerChoice === "Rock") {
+//     if (computerChoice === "Paper") {
+//         return `${playerChoice} vs. ${computerChoice}! You lose!`;
+//     } else {
+//         return `${playerChoice} vs. ${computerChoice}! You win!`;
+//     }
+// } else if (playerChoice === "Paper") {
+//     if (computerChoice === "Rock") {
+//         return `${playerChoice} vs. ${computerChoice}! You win!`;
+//     } else {
+//         return `${playerChoice} vs. ${computerChoice}! You lose!`;
+//     }
+// } else {
+//     if (computerChoice === "Rock") {
+//         return `${playerChoice} vs. ${computerChoice}! You lose!`;
+//     } else {
+//         return `${playerChoice} vs. ${computerChoice}! You win!`;
+//     }
+// }
+
 
 const choice = ["Rock", "Paper", "Scissors"];
 const playerChoice = getPlayerChoice();
