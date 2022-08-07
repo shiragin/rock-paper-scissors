e34329501532459bdda885240c2c4d23a5135d1f
Shira Ginat <shiragin@gmail.com>
2022-08-07 03:20:55 +0300

    Build getPlayerChoice w/ capitals & content check

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 "use strict";
 
-const choice = ["Rock", "Paper", "Scissors"];
-const playerChoice = "Rock";
-const computerChoice = getComputerChoice();
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Choose - Paper, Rock or Scissors?");
+    playerChoice = capitalizeFirstLetter(playerChoice);
+    if (!choice.includes(playerChoice)) {
+        console.log("Your choice was invalid. Please choose again.");
+        return getPlayerChoice();
+    } else {
+        return playerChoice;
+    }
+}
 
 function getComputerChoice() {
     return choice[Math.floor(Math.random() * choice.length)];
 }
+
+// function playRound(playerChoice, computerChoice) {
+//     if (playerChoice === "")
+// }
+
+const choice = ["Rock", "Paper", "Scissors"];
+const playerChoice = getPlayerChoice();
+const computerChoice = getComputerChoice();
 
 console.log(playerChoice, computerChoice);
 
