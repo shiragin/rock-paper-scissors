a2624a9c32612730cac4ac7497f54b71cf6c611e
Shira Ginat <shiragin@gmail.com>
2022-08-07 03:02:46 +0300

    Declare variables for computer and player choice

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 "use strict";
 
 const choice = ["Rock", "Paper", "Scissors"];
+const playerChoice = "Rock";
+const computerChoice = getComputerChoice();
 
 function getComputerChoice() {
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
-console.log(getComputerChoice());
+console.log(playerChoice, computerChoice);
+
+
 
