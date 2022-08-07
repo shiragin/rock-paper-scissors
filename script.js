2d3f8e97bce8358060df3fb22b1a4191b58fd0cf
Shira Ginat <shiragin@gmail.com>
2022-08-07 03:37:27 +0300

    Finish playRound function w/ choice & win/lose msg

--- script.js
+++ script.js
@@ -19,15 +19,39 @@ function getComputerChoice() {
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
-// function playRound(playerChoice, computerChoice) {
-//     if (playerChoice === "")
-// }
+function playRound(playerChoice, computerChoice) {
+    console.log(`You chose... ${playerChoice}!`);
+    console.log(`The evil computer chose... ${computerChoice}!`);
+    if (playerChoice === computerChoice) {
+        return `${playerChoice} vs. ${computerChoice}! It's a draw!`;
+    } else {
+        if (playerChoice === "Rock") {
+            if (computerChoice === "Paper") {
+                return `${playerChoice} vs. ${computerChoice}! You lose!`;
+            } else {
+                return `${playerChoice} vs. ${computerChoice}! You win!`;
+            }
+        } else if (playerChoice === "Paper") {
+            if (computerChoice === "Rock") {
+                return `${playerChoice} vs. ${computerChoice}! You win!`;
+            } else {
+                return `${playerChoice} vs. ${computerChoice}! You lose!`;
+            }
+        } else {
+            if (computerChoice === "Rock") {
+                return `${playerChoice} vs. ${computerChoice}! You lose!`;
+            } else {
+                return `${playerChoice} vs. ${computerChoice}! You win!`;
+            }
+        }
+    }
+}
 
 const choice = ["Rock", "Paper", "Scissors"];
 const playerChoice = getPlayerChoice();
 const computerChoice = getComputerChoice();
 
-console.log(playerChoice, computerChoice);
+console.log(playRound(playerChoice, computerChoice));
 
 
 
